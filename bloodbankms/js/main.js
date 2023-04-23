bf5b7b4ca3f429caf765be11c184a82097d74185
John Dexter Lanot <21-09401@g.batstate-u.edu.ph>
2023-04-23 19:28:12 +0800

    eyy T_T

---
+++ bloodbankms/js/main.js
@@ -0,0 +1,30 @@
+let index = 0;
+const totalWorkItems = $(".work-item").length; 
+
+ $(window).on("load",function(){
+    $(".preloader").addClass("loaded");
+ })
+
+
+$(document).ready(function () {
+
+    // nav toggle
+    $(".nav-toggle").click(function(){
+      $(".header .nav").slideToggle();
+    })
+    $(".header .nav a").click(function(){
+      if($(window).width() < 768){
+         $(".header .nav").slideToggle();
+      }
+    })
+
+    // fixed header 
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 100){
+        $(".header").addClass("fixed");
+      }
+      else{
+        $(".header").removeClass("fixed");
+      }
+    })
+})
